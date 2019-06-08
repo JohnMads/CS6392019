4427f59753a9cc98d2a0a7f9e3a9dc3b5a1ed633
JohnMads <johnpmadias@gmail.com>
2019-06-08 14:51:13 -0400

    Adding UISummerI

---
+++ UISummerI/UISummerI/app/src/main/java/com/example/uisummeri/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.uisummeri;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_main_activity);
+    }
+}
