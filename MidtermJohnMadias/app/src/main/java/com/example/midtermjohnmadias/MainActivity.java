551d4f41910ee1986fbe6fd12190f064b0642486
JohnMads <johnpmadias@gmail.com>
2019-06-25 19:18:24 -0400

    Adding Midterm

---
+++ MidtermJohnMadias/app/src/main/java/com/example/midtermjohnmadias/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.midtermjohnmadias;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.example.midtermjohnmadias.MESSAGE";
+
+    public void conversion(View view) {
+        EditText amount =(EditText) findViewById(R.id.milesAmount);
+        Double milesAmount = Double.parseDouble(amount.getText().toString());
+        Double kmsAmount= milesAmount * 1.609;
+        Toast.makeText(getApplicationContext(),"Miles to Kms conversion is: "+ kmsAmount.toString(), Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
